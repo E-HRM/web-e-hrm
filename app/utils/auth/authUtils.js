c63a2332c58c83954a547054f3340f73bebcca6f
dewade17 <andrenewer@gmail.com>
2025-08-25 10:38:25 +0800

    feat: initiation-project

--- app/utils/auth/authUtils.js
+++ app/utils/auth/authUtils.js
@@ -1,17 +1,13 @@
-// import { NextResponse } from "next/server";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 
+export async function authenticateRequest() {
+  const session = await getServerSession(authOptions);
 
-// export async function authenticateRequest() {
-//   const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ status: 401, message: 'Unauthorized: You must be logged in' }, { status: 401 });
+  }
 
-//   if (!session) {
-//     return NextResponse.json(
-//       { status: 401, message: "Unauthorized: You must be logged in" },
-//       { status: 401 }
-//     );
-//   }
-
-//   return session.user;
-// }
+  return session.user;
+}
