be18621d840d0ca1ef23e86958f33eff2d828111
yogantaraz <aditya.yogantaraz@gmail.com>
2025-10-25 17:48:56 +0800

    feat: delete history migrate

--- prisma/migrations/20251025080420_ubah_status_agenda_kerja_teragenda/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status_kunjungan` on the `kunjungan` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(28))` to `Enum(EnumId(2))`.
-
-*/
--- AlterTable
-ALTER TABLE `kunjungan` MODIFY `status_kunjungan` ENUM('diproses', 'berlangsung', 'selesai') NOT NULL DEFAULT 'diproses';
