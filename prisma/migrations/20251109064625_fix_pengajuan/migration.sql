f5bb822ba8bdc82b94cc9c76c4ab95134d1949b1
yogantaraz <aditya.yogantaraz@gmail.com>
2025-11-26 17:41:36 +0800

    feat: v2

--- prisma/migrations/20251109064625_fix_pengajuan/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `cuti_konfigurasi` MODIFY `cuti_tabung` INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE `user` MODIFY `status_cuti` ENUM('aktif', 'nonaktif') NOT NULL DEFAULT 'nonaktif';
--- prisma/migrations/20251110064828_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `kategori_cuti` ADD COLUMN `pengurangan_kouta` BOOLEAN NOT NULL DEFAULT true;
